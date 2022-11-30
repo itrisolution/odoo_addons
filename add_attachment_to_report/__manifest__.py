72754299776dda57dbf855e3e5d9a88fba9339eb
ma@syentys.fr <ma@syentys.fr>
2022-11-30 23:27:48 +0100

    [ADD] add_attachment_to_report v 14.0

---
+++ add_attachment_to_report/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Add Attachment to Report',
+    'description': """
+            This module will help you to add attachment to report.
+        """,
+    'summary': 'Module to help you add attachment to report',
+    'author': 'Imal-Tech',
+    'website': 'https://www.imal-tech.com',
+    'maintainer': 'Imal-Tech',
+    'category': 'Tools',
+    'version': '15.0.1.0.0',
+    'depends': ['base', 'mail'],
+    'data': [
+        'data/security.xml',
+        'views/res_company_views.xml',
+    ],
+    'license': 'LGPL-3',
+    # 'images': ['static/description/banner.gif'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'price': '2.0',
+    'currency': 'USD',
+}
---
+++ add_attachment_to_report/models/res_company.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, _, api
+
+
+class ResCompany(models.Model):
+    """ Inherit Res Company to add related_model_ids """
+    _inherit = 'res.company'
+
+    related_model_ids = fields.Many2many('ir.model', string="Models", domain="[('transient', '=', False), ('is_mail_thread', '=', True)]")
+
